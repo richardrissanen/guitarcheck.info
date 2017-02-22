6561b951db52ce2c08f0e818c769ea3ab497220e
Richard Rissanen <richardrissanen@me.com>
2017-02-22 13:18:41 -0600

    Added tests and data for japan

--- js/lib/countries/japan.js
+++ js/lib/countries/japan.js
@@ -5,12 +5,35 @@ define(['../utility'], function(utility) {
 
   var japanModule = function() {
 
-    // data = {
-    //   maximumPrefixSize: #,
-    //   obj: {
-    //
-    //   }
-    // }
+    data = {
+      minimumPrefixSize: 1,
+      maximumPrefixSize: 2,
+      prefixes: {
+        'a': '(mij) 1985 - 1986 | (cij) 1997 - 1998',
+        'b': '(mij) 1985 - 1986 | (cij) 1998 - 1999',
+        'c': '1985 - 1986',
+        'e': '1984 - 1987',
+        'f': '1986 - 1987',
+        'g': '1987 - 1988',
+        'h': '1988 - 1989',
+        'i': '1989 - 1990',
+        'j': '1989 - 1990',
+        'jv': '1982 - 1984',
+        'k': '1990 - 1991',
+        'l': '1991 - 1992',
+        'm': '1992 - 1993',
+        'n': '(mij) 1993 - 1994 | (cij) 1995 - 1996',
+        'o': '1997 - 1998',
+        'p': '(mij) 1993 - 1994 | (cij) 1999 - 2002',
+        'q': '(mij) 1993 - 1994 | (cij) 2002 - 2004',
+        'r': '2004 - 2006',
+        's': '(mij) 1994 - 1995 | (cij) 2006 - 2008',
+        'sq': '1983 - 1984',
+        't': '(mij) 1994 - 1995 | (mij "post 2006") 2007 - 2010',
+        'u': '(mij) 1995 - 1996 | (mij "post 2006") 2010 - 2011',
+        'v': '1996 - 1997'
+      }
+    }
 
     // Iterates over possible serial numbers from Japan and checks against the
     // serial number.
@@ -19,7 +42,20 @@ define(['../utility'], function(utility) {
     //   FAILURE PATH: returns { status: 'ok', state: false, message: null }
     //   ERROR PATH:   returns { status: 'error', state: false, message: <error> }
     this.isSerialNumberFromJapan = function(serialNumber) {
-      return utility.createMessageObject('ok', true, 'japan')
+      // count down from max size so SQ will run before S. Otherwise S will
+      // catch all SQ serial numbers. Same for JV before J
+      for (var i = data.maximumPrefixSize; i >= data.minimumPrefixSize; i--) {
+
+        serialNumberPrefix = serialNumber.substring(0, i)
+        message = data.prefixes[serialNumberPrefix]
+
+        console.log(serialNumberPrefix);
+        console.log('message: ' + message);
+
+        if (typeof message !== 'undefined') { break }
+      }
+
+      return utility.createMessageObject('ok', true, message)
     }
 
   }
--- js/tests/test.js
+++ js/tests/test.js
@@ -59,13 +59,162 @@ requirejs(['simpleTest', '../lib/serialNumberChecker', '../config/strings'], fun
       simpleTest.assert(message.state   === true)
       simpleTest.assert(message.message === 'indonesia')
     },
-    testProcessFormForJapan: function() {
-      var message = serialNumberChecker.processForm('JV12345', 'japan')
+    testProcessFormForJapanA: function() {
+      var message = serialNumberChecker.processForm('a123456', 'japan')
 
       simpleTest.assert(message.status  === 'ok')
       simpleTest.assert(message.state   === true)
-      simpleTest.assert(message.message === 'japan')
+      simpleTest.assert(message.message === '(mij) 1985 - 1986 | (cij) 1997 - 1998')
     },
+    testProcessFormForJapanB: function() {
+      var message = serialNumberChecker.processForm('b123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '(mij) 1985 - 1986 | (cij) 1998 - 1999')
+    },
+    testProcessFormForJapanC: function() {
+      var message = serialNumberChecker.processForm('c123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '1985 - 1986')
+    },
+    testProcessFormForJapanE: function() {
+      var message = serialNumberChecker.processForm('e123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '1984 - 1987')
+    },
+    testProcessFormForJapanF: function() {
+      var message = serialNumberChecker.processForm('f123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '1986 - 1987')
+    },
+    testProcessFormForJapanG: function() {
+      var message = serialNumberChecker.processForm('g123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '1987 - 1988')
+    },
+    testProcessFormForJapanH: function() {
+      var message = serialNumberChecker.processForm('h123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '1988 - 1989')
+    },
+    testProcessFormForJapanI: function() {
+      var message = serialNumberChecker.processForm('i123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '1989 - 1990')
+    },
+    testProcessFormForJapanJ: function() {
+      var message = serialNumberChecker.processForm('j123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '1989 - 1990')
+    },
+    testProcessFormForJapanJV: function() {
+      var message = serialNumberChecker.processForm('jv12345', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '1982 - 1984')
+    },
+    testProcessFormForJapanK: function() {
+      var message = serialNumberChecker.processForm('k123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '1990 - 1991')
+    },
+    testProcessFormForJapanL: function() {
+      var message = serialNumberChecker.processForm('l123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '1991 - 1992')
+    },
+    testProcessFormForJapanM: function() {
+      var message = serialNumberChecker.processForm('m123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '1992 - 1993')
+    },
+    testProcessFormForJapanN: function() {
+      var message = serialNumberChecker.processForm('n123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '(mij) 1993 - 1994 | (cij) 1995 - 1996')
+    },
+    testProcessFormForJapanP: function() {
+      var message = serialNumberChecker.processForm('p123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '(mij) 1993 - 1994 | (cij) 1999 - 2002')
+    },
+    testProcessFormForJapanQ: function() {
+      var message = serialNumberChecker.processForm('q123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '(mij) 1993 - 1994 | (cij) 2002 - 2004')
+    },
+    testProcessFormForJapanR: function() {
+      var message = serialNumberChecker.processForm('r123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '2004 - 2006')
+    },
+    testProcessFormForJapanS: function() {
+      var message = serialNumberChecker.processForm('s123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '(mij) 1994 - 1995 | (cij) 2006 - 2008')
+    },
+    testProcessFormForJapanSQ: function() {
+      var message = serialNumberChecker.processForm('sq12345', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '1983 - 1984')
+    },
+    testProcessFormForJapanT: function() {
+      var message = serialNumberChecker.processForm('t123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '(mij) 1994 - 1995 | (mij "post 2006") 2007 - 2010')
+    },
+    testProcessFormForJapanU: function() {
+      var message = serialNumberChecker.processForm('u123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '(mij) 1995 - 1996 | (mij "post 2006") 2010 - 2011')
+    },
+    testProcessFormForJapanV: function() {
+      var message = serialNumberChecker.processForm('v123456', 'japan')
+
+      simpleTest.assert(message.status  === 'ok')
+      simpleTest.assert(message.state   === true)
+      simpleTest.assert(message.message === '1996 - 1997')
+    },
+
+
     testProcessFormForKoreaCN: function() {
       var message = serialNumberChecker.processForm('cn423456', 'korea')
 
