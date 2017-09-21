6692bb2ef5c8c2fb6f76654555f9fe0c968d01cc
Richard Rissanen <richardrissanen@me.com>
2017-09-21 13:32:20 -0500

    Updated Japan data to be more human readable and added implementation for on submit

--- guitarcheck/src/app/serial-number-form/serial-number-form.component.ts
+++ guitarcheck/src/app/serial-number-form/serial-number-form.component.ts
@@ -14,17 +14,34 @@ export class SerialNumberFormComponent implements OnInit {
   countries: Array<string>
   countryOfOrigin: string
   serialNumber: string
+  company = 'squier'
 
   constructor(private dataService: SerialNumberDataService) { }
 
   ngOnInit() {
-    this.manufacturerData = this.dataService.fetch_company('squier');
-    this.countries = this.dataService.fetch_countries_for_a_company('squier')
+    this.manufacturerData = this.dataService.fetch_company(this.company);
+    this.countries = this.dataService.fetch_countries_for_a_company(this.company)
   }
 
   onSubmit() {
     // compare input to manufacturerData and display result
-    console.log(this.countryOfOrigin)
+    const origin = this.manufacturerData[this.countryOfOrigin]
+    const minSize = origin.minimumPrefixSize
+    const maxSize = origin.maximumPrefixSize
+
+    let year;
+    
+    for (var i = maxSize; i >= minSize; i--) {
+
+      const serialNumberPrefix = this.serialNumber.substring(0, i)
+      year = origin[serialNumberPrefix]
+
+      if (typeof year !== 'undefined') { break }
+    }
+
+    const message = 'Your ' + this.company + ' guitar is from ' + year
+    alert(message)
+    
   }
 
 }
